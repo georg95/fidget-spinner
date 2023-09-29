9c465d59f9a4652f8fc54f293153531907df821b
Georgy <georgsh-yateam@yandex.com>
2023-09-29 22:54:25 +0300

    add more smart and stable audio and pics load systems

---
+++ scripts/audio.js
@@ -0,0 +1,79 @@
+var use_sound = true;
+
+document.addEventListener('DOMContentLoaded', initSound);
+
+var switchSound = function() {};
+
+function initSound()
+  {
+  var safari_sound_enabled = false;
+  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  console.log('audio:', audioCtx);
+  var gainNode = audioCtx.createGain();
+  var source;
+  var sounds = [];
+  switchSound = function()
+    {
+    use_sound = !use_sound
+    if(use_sound) { gainNode.gain.value = 1; }
+    else          { gainNode.gain.value = 0; }
+    }
+
+  function enableSoundOnSafari()
+    {
+    if(safari_sound_enabled) { return; }
+    var buffer = audioCtx.createBuffer(1, 1, 22050);
+  	var source = audioCtx.createBufferSource();
+  	source.buffer = buffer;
+  	source.connect(audioCtx.destination);
+  	if(source.noteOn) { source.noteOn(0); }
+    safari_sound_enabled = true;
+    }
+
+  function enableSound()
+    {
+    enableSoundOnSafari();
+    playSound(sounds[0]);
+    }
+  window.addEventListener('touchstart', enableSound, false);
+  window.addEventListener('mousedown', enableSound, false);
+
+  function playSound(buffer)
+    {
+    if(source || !buffer) { return; }
+    console.log('play sound');
+    source = audioCtx.createBufferSource();
+    source.buffer = buffer;
+    source.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+    source.loop = true;
+    source.start(0);
+    }
+
+  function loadSounds()
+    {
+    console.log('loading sounds...');
+    loadSound('YouSpinMeRound1.ogg', function(sound1)
+      {
+      sounds = [sound1];
+      });
+    }
+
+  function loadSound(name, callback)
+    {
+    function onError(err)
+      {
+      console.log('error ocurred:', err)
+      }
+    var request = new XMLHttpRequest();
+    request.open('GET', name, true);
+    request.responseType = 'arraybuffer';
+    request.onload = function() {
+      audioCtx.decodeAudioData(request.response, function(buffer) {
+        callback(buffer);
+        }, onError);
+      }
+    request.send();
+    }
+  loadSounds();
+  }
--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,5 @@
-console.log('hello world');
-var use_sound = true;
-var use_vibro = true;
 
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-console.log('audio:', audioCtx);
-var gainNode = audioCtx.createGain();
+var use_vibro = true;
 
 (function() {
   var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
@@ -52,24 +47,13 @@ function drawSpinner(canvas, images)
   ctx.font = "30pt Arial";
   function dst(x,y,x2,y2)
     { return Math.sqrt((x-x2)*(x-x2)+(y-y2)*(y-y2)); }
-  function cos2(x,y,x2,y2)
-    { return (x*x2+y*y2)/dst(0,0,x,y)/dst(0,0,x2,y2); }
-  function spd(x,y,x2,y2)
-    {
-    var a = Math.atan2(y2-y, x2-x);
-    var b = Math.atan2(y-H/2, x-H/2)+Math.PI/2;
-    console.log('angle1:', a/Math.PI*180);
-    console.log('angle2:', b/Math.PI*180);
-    return dst(x,y,x2,y2)*Math.cos(b-a);
-    }
   function onDrag(x,y)
     {
-    console.log('drag at', x, y);
     for(var i = 0; i < 3; i++)
       {
       var cx = W/2+R*Math.cos(rotAngle-Math.PI/2+i*Math.PI*2/3);
       var cy = H/2+R*Math.sin(rotAngle-Math.PI/2+i*Math.PI*2/3);
-      if(dst(x,y,cx,cy) < R/2) { console.log('drag successful'); return true; }
+      if(dst(x,y,cx,cy) < R/2) { return true; }
       }
     return false;
     }
@@ -81,15 +65,10 @@ function drawSpinner(canvas, images)
   function checkAudio(x,y)
     {
     if (x > 0 && x < W/7 && y > 0 && y < H/7)
-      {
-      use_sound = !use_sound
-      if(use_sound) { gainNode.gain.value = 1; }
-      else          { gainNode.gain.value = 0; }
-      }
+      { switchSound(); }
     }
   function takeSpinner(e)
     {
-    if(!source) { playSound(sounds[0]); }
     e.preventDefault();
     //console.log(e);
     var ex = eventX(e);
@@ -115,8 +94,6 @@ function drawSpinner(canvas, images)
     if(movespeeds.length !== 0)
        { speed = movespeeds.reduce(function(a,b)
           { return Math.abs(a)>Math.abs(b)?a:b; }, 0); }
-    console.log('speeds:', movespeeds);
-    console.log('speed:', speed);
     }
   function flipSpinner(e)
     {
@@ -178,8 +155,6 @@ function drawSpinner(canvas, images)
     ctx.rotate(rotAngle);
     ctx.drawImage(images['spinner'], 0, 0, 530, 530, -W/2, -H/2, W, H);
     ctx.setTransform(1, 0, 0, 1, 0, 0);
-    // ctx.fillStyle='#0F0';
-    // ctx.fillRect(0,0,spincounter,10);
     ctx.fillStyle='#999';
     ctx.strokeStyle = "#999";
     ctx.translate(W/2, 15);
@@ -191,117 +166,52 @@ function drawSpinner(canvas, images)
       if(rotateDeg < -360) { while(rotateDeg < -360) { rotateDeg+=360; spincounter++; } vib(); } }
     rotAngle = rotateDeg*Math.PI/180;
     function vib() { if(use_vibro && "vibrate" in navigator) navigator.vibrate(6); }
-    // if(cur_sound === 0 && spincounter > 250)
-    //   {
-    //   source.loop = false;
-    //   source.onended = function()
-    //       {
-    //       playSound(sounds[++cur_sound]);
-    //       }
-    //   }
-    // TODO sound switch
     requestAnimationFrame(redraw);
     }
   requestAnimationFrame(redraw);
   }
 
-function loadPic()
+function countDown(len, callback)
+  {
+  return function()
+    { if(--len === 0) { callback(); } }
+  }
+
+function loadPics(base, pics, callback)
+  {
+  var countDownCallback = countDown(pics.length, function() { callback(imgbase); });
+  var imgbase = {};
+  pics.forEach(function(name)
+    {
+    var img = new Image();
+    img.src = base+name;
+    function addNewImage()
+      {
+      imgbase[name.slice(0, name.lastIndexOf('.'))] = img;
+      countDownCallback();
+      }
+    img.onload = addNewImage;
+    img.onerror = function()
+      {
+      console.log('error while loading', base+name);
+      addNewImage(); // btw we don't want break all
+      }
+    })
+  }
+
+function loadGame()
   {
   var canvas = createCanvas();
   document.body.appendChild(canvas);
-  var spinnerImg = new Image();
-  spinnerImg.src = 'spinner.png';
-  spinnerImg.onload = function()
-    {
-    var spinnerButton = new Image();
-    spinnerButton.src = 'button.png';
-    spinnerButton.onload = function()
+  loadPics('pics/',
+    ['spinner.png',
+     'button.png',
+     'speaker_on.png',
+     'speaker_off.png',
+     'vibro_on.png',
+     'vibro_off.png'], function(pics)
       {
-      var soundOn = new Image();
-      soundOn.src = 'speaker_on.png';
-      soundOn.onload = function()
-        {
-        var soundOff = new Image();
-        soundOff.src = 'speaker_off.png';
-        soundOff.onload = function()
-          {
-          var vibroOn = new Image();
-          vibroOn.src = 'vibro_on.png';
-          vibroOn.onload = function()
-            {
-            var vibroOff = new Image();
-            vibroOff.src = 'vibro_off.png';
-            vibroOff.onload = function()
-              {
-              loadSounds(function()
-                {
-                drawSpinner(canvas,
-                    {
-                    'spinner': spinnerImg,
-                    'button': spinnerButton,
-                    'speaker_on': soundOn,
-                    'speaker_off': soundOff,
-                    'vibro_on': vibroOn,
-                    'vibro_off': vibroOff
-                    });
-                })
-              }
-            }
-          }
-        }
-      }
-    }
+      drawSpinner(canvas, pics);
+      });
   }
-document.addEventListener('DOMContentLoaded', loadPic);
-var source;
-var sounds = [];
-var cur_sound = 0;
-
-window.addEventListener('touchstart', function()
-  {
-	var buffer = audioCtx.createBuffer(1, 1, 22050);
-	var source = audioCtx.createBufferSource();
-	source.buffer = buffer;
-	source.connect(myContext.destination);
-	source.noteOn(0);
-
-  playSound(sounds[0]);
-  }, false);
-
-function playSound(buffer)
-  {
-  console.log('play sound');
-  source = audioCtx.createBufferSource();
-  source.buffer = buffer;
-  source.connect(gainNode);
-  gainNode.connect(audioCtx.destination);
-  source.loop = true;
-  source.start(0);
-  }
-
-function loadSound(name, callback)
-  {
-  function onError(err)
-    {
-    console.log('error ocurred:', err)
-    }
-  var request = new XMLHttpRequest();
-  request.open('GET', name, true);
-  request.responseType = 'arraybuffer';
-  request.onload = function() {
-    audioCtx.decodeAudioData(request.response, function(buffer) {
-      callback(buffer);
-      }, onError);
-    }
-  request.send();
-  }
-
-function loadSounds(callback)
-  {
-  console.log('loading sounds...');
-  loadSound('YouSpinMeRound1.ogg', function(sound1)
-    {
-    sounds = [sound1];
-    callback();
-    });
-  }
+document.addEventListener('DOMContentLoaded', loadGame);
